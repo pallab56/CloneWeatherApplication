f7ecbd08c920339803133595721da80c43d232df
pallab56 <pallab15-5698@diu.edu.bd>
2026-07-11 21:26:44 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/rivaanranawaatweatherapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.rivaanranawaatweatherapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      home: const HopmePage(),
+    );
+  }
+}
+
+class HopmePage extends StatelessWidget {
+  const HopmePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
